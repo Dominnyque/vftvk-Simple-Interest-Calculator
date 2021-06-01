0abd7b4bb780ccf25a3ec3052f40a7b5f8b52ba9
Dominique <59777085+Dominnyque@users.noreply.github.com>
2021-06-01 23:44:07 +0300

    Update script.js

--- script.js
+++ script.js
@@ -1,3 +1,4 @@
+
 function compute(){
    var p = document.getElementById("principal").value;
    var r = document.getElementById("rate").value;
@@ -7,5 +8,5 @@ function compute(){
    amount = parseInt(p)+parseFloat(interest);
    var timing = Number(2020) + Number(t);
 
-document.getElementById("result").innerHTML = " If you deposit "+ p +" at the interest "+ r + " You will receive an amount of " +interest + " in the year " + timing;
+document.getElementById("result").innerHTML = " If you deposit "+ p +" at the interest of "+ r+"%" + " You will receive an amount of " +interest + " in the year " + timing;
 }
