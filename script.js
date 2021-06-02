db6780f0dedbf7beb668407dc53922fc56637b90
Dominique <59777085+Dominnyque@users.noreply.github.com>
2021-06-02 12:07:04 +0300

    Updated alert
    updated from line 14 to 20

--- script.js
+++ script.js
@@ -1,7 +1,9 @@
+
 function compute(){
    var p = document.getElementById("principal").value;
    var r = document.getElementById("rate").value;
    var t = document.getElementById("years").value;
+
    var pd= parseInt(t)
 
    var interest = Number(p*r* (pd)/100);
@@ -9,7 +11,10 @@ function compute(){
    var timing = Number(2020) + (pd);
 if (p>0){
 document.getElementById("result").innerHTML = " If you deposit "+ p +" at the interest of "+ r+"%" + " You will receive an amount of " +interest + " in the year " + timing;
-} else{
-   alert("Principal amount must be a positive number >0");
+} else if (p==0){
+   alert("Principal amount cannot be Zero ( 0 )");
+}
+else if (p<0){
+   alert("Please enter a positive number");
 }
 }
